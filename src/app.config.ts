bc42827c13c9992a8c789d15801255cf8bf56e25
roadloser <>
2020-11-25 18:49:45 +0800

    项目初始化

---
+++ src/app.config.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: roadloser
+ * @Date: 2020-11-25 15:23:19
+ * @LastEditors: roadloser
+ * @LastEditTime: 2020-11-25 18:48:21
+ */
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
